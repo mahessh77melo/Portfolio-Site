a734238e79a8ce9c69b5426f3218aa3cd9adce5d
mahessh77melo <magesh.lbj23@gmail.com>
2020-12-10 21:41:16 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import "../Styles/App.scss";
+
+function App() {
+	return (
+		<div className="app">
+			<Home />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Styles/Home.scss";
+
+const Home = () => {
+	return (
+		<div className="home">
+			{/* <Nav /> */}
+			<div className="center">
+				<div className="center__title">Hi, this is Mahesh J</div>
+				<div className="center__description">
+					Trying to put a smile on everyone's face.
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
