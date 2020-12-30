fba7a58cda199967ee80abd0880a0b075633faf6
mahessh77melo <magesh.lbj23@gmail.com>
2020-12-30 16:58:07 +0530

    project page setup

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,42 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../Styles/Projects.scss";
 
 const Projects = () => {
-	return <div>Projects page</div>;
+	return (
+		<div className="projects">
+			<div className="project">
+				<div className="project__title">Hulu Clone</div>
+				<div className="project__domain">Web Dev</div>
+				<div className="project__tools">
+					<div className="project__tools-item">React JS</div>
+					<div className="project__tools-item">TMDB api</div>
+					<div className="project__tools-item">React Router</div>
+				</div>
+				<a
+					href="https://github.com/mahessh77melo/hulu-clone"
+					className="project__link"
+				>
+					<GitHubIcon />
+				</a>
+			</div>
+			<div className="project">
+				<div className="project__title">Hulu Clone</div>
+				<div className="project__domain">Web Dev</div>
+				<div className="project__tools">
+					<div className="project__tools-item">React JS</div>
+					<div className="project__tools-item">TMDB api</div>
+					<div className="project__tools-item">React Router</div>
+				</div>
+				<a
+					href="https://github.com/mahessh77melo/hulu-clone"
+					className="project__link"
+				>
+					<GitHubIcon />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
